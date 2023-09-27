549414144169b9afa438158c8baa7efd944c3310
Iss25 <114648812+Iss25@users.noreply.github.com>
2023-09-27 15:48:05 +0200

    exo 1.2.2

--- src/main/java/ex_supp/Ex1_2_2.java
+++ src/main/java/ex_supp/Ex1_2_2.java
@@ -1,11 +1,34 @@
 package ex_supp;
 
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Ex1_2_2 {
-    public int eval_PostFIX(){
-        return 0;
+    public static int eval_PostFIX(StringTokenizer token){
+        Stack values  = new Stack<Integer>();
+        while (token.hasMoreTokens()) {
+            String element = token.nextToken();
+            if(element.equals("+")){
+                int val1 = Integer.parseInt((String) values.pop());
+                int val2 = Integer.parseInt((String) values.pop());
+                values.push(String.valueOf(val1+val2));
+            }
+            else if(element.equals(("*"))){
+                int val1 = Integer.parseInt((String) values.pop());
+                int val2 = Integer.parseInt((String) values.pop());
+                values.push(String.valueOf(val1*val2));
+            }
+            else{
+                values.push(element);
+            }
+        }
+        return Integer.parseInt((String) values.pop());
     }
 
     public static void main(String[] args) {
-        System.out.println("ok");
+        String in = "4 20 + 3 5 1 * * +";
+        StringTokenizer tokenizer = new StringTokenizer(in);
+        int res = eval_PostFIX(tokenizer);
+        System.out.println(res);
     }
 }
